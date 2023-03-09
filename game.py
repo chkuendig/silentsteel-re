9ce3d2e77cbfea8d7147418e625d7ced6425cb1a
Christian Kündig <christian@kuendig.info>
2023-03-09 15:13:33 +0100

    Integrating handling user choice, voice playback and picking the right video after. Still buggy when it comes to jumps ($j commands)

--- game.py
+++ game.py
@@ -1,9 +1,11 @@
-import pprint
 import json
 import sys
-from play_idx import play_audio, play_video 
-SKIP=-1
-CONTINUE=0
+import random
+from play_idx import play_audio, play_video
+SKIP = -1
+CONTINUE = 0
+
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -15,86 +17,120 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 folder = "/Volumes/Untitled/"
+
+
 def parse(data):
     parts = []
     string = ""
-    while(len(data)>0):
+    while (len(data) > 0):
         byte = data.pop(0)
         char = chr(byte)
 
-        if(char == "{"):
-            if(len(string.strip()) > 0):
+        if (char == "{"):
+            if (len(string.strip()) > 0):
                 parts.append(string.strip())
             parts.append(parse(data))
             string = ""
-        elif(char == "}"):       
-            if(len(string.strip()) > 0):
+        elif (char == "}"):
+            if (len(string.strip()) > 0):
                 parts.append(string.strip())
+            if (len(parts) == 1):
+                return parts[0]
             return parts
         else:
             string += char
     return parts
 
-def executeCmd(cmd):
-    if(cmd[0:3] == "?r$" and cmd[-1] == ";"):
-        play_video(int(cmd[3:-1]))
 
-    elif(cmd[0:2] == "?j"):
+def executeCmd(cmd):
+    cmd_parts = cmd.split(";")
+
+    if (len(cmd_parts) > 1 and len(cmd_parts[1].strip()) > 0):
+        print("Message: %s" % cmd_parts[1])
+    cmd = cmd_parts[0]
+    if (cmd[0:3] == "?r$"):
+        play_video(int(cmd[3:]))
+
+    elif (cmd[0:2] == "?j"):
         play_video(int(cmd[2:]))
         return SKIP
     else:
-        print(cmd[1:2])
-        print("Unsupported command: %s"%cmd)
+        print("Unsupported command: %s" % cmd)
+
     return CONTINUE
 
+
 def playSequence(sequence):
-    cmds = sequence.split(" > ")
-    print(cmds);
+    cmds = sequence.split(">")
+    print(cmds)
     for cmd in cmds:
-        if(len(cmd)>0):
+        cmd = cmd.strip()
+        if (len(cmd) > 0):
             ret = executeCmd(cmd)
-            if(ret == SKIP):
-                return 
+            if (ret == SKIP):
+                return
+
 
 def runExchange(exchange):
     options = {}
     lastOption = ""
     for line in exchange:
-        print(line)
-        if(line[0] in ["+","=","-"]):
+        if (line[0] in ["+", "=", "-"]):
             options[line] = []
             lastOption = line
-        elif(line[0] == ">"):
+        elif (line[0] == ">"):
             options[lastOption].append(line)
-    
-    print(options.keys())
-    input("Press Enter to continue...")
+        else:
+            print("ASSSD %s" % line)
+    for idx, key in enumerate(options.keys()):
+        print("%i. %s" % (idx+1, key[7:]))
+    choice_idx = -1
+    while (choice_idx < 0 or choice_idx > 2):
+        inputstr = input("Your Choice:")
+        if (inputstr.isdigit()):
+            choice_idx = int(inputstr)-1
+
+    choice_str = list(options.keys())[choice_idx]
+    audio_idx = choice_str[3:7]
+    play_audio(int(audio_idx))
+
+    result_str = random.choice(options[choice_str])
+    cmds = result_str.split(">")
+    for cmd in cmds:
+        cmd = cmd.strip()
+        if (len(cmd) > 0):
+            ret = executeCmd(cmd)
+            if (ret == SKIP):
+                return SKIP
+
 
 with open(folder + "STEEL.EXE", 'rb') as fin:
     fin.seek(93805)
     data = fin.read(330130-93805)
-    parts = parse(bytearray(data)[0:15000])
+    scenes = parse(bytearray(data))
     exchange = -1
     skipExchange = False
-    for part in parts:
-        
-        if(part[0].startswith("sequence")):
+    skip = 0
+    if (len(sys.argv) >= 2 and sys.argv[1].isdigit()):
+        scenes = scenes[int(sys.argv[1]):]
+    for scene in scenes:
+        if (isinstance(scene, str) and scene.startswith("sequence")):
             skipExchange = False
-            playSequence(part[0][8:])
-            #input("Press Enter to continue...")
-        elif isinstance(part, str) and part.endswith('EXCHANGE'):
+            print(scene)
+            playSequence(scene[8:])
+        elif isinstance(scene, str) and scene.endswith('EXCHANGE'):
             if not skipExchange:
-                exchange = int(part[0:-9])
+                exchange = int(scene[0:-9])
             else:
-                print("Skipping exchange" + part)
-        elif exchange >0:
-            ret = runExchange(part)
-            if(ret == SKIP):
+                print("Skipping exchange" + scene)
+        elif exchange > 0:
+            ret = runExchange(scene)
+            if (ret == SKIP):
                 skipExchange = True
             exchange = -1
-            
-            
-        else:
-             print(bcolors.BOLD+"WARNING: "+bcolors.OKBLUE+json.dumps(part)+ bcolors.ENDC)
 
+        elif not skipExchange:
+            print(bcolors.BOLD+"UNSUPPORTED SCENE: " +
+                  bcolors.OKBLUE+json.dumps(scene) + bcolors.ENDC)
--- play_idx.py
+++ play_idx.py
@@ -3,47 +3,46 @@ import sys
 import wave
 from subprocess import run
 folder = "/Volumes/Untitled/"
-def parseIdx(filename): 
+
+
+def parseIdx(filename):
     parts = {}
     lastIdx = -1
     print(filename)
-    with open(filename ,'rb') as in_file:
+    with open(filename, 'rb') as in_file:
         print("====================")
         print(filename)
         data = in_file.read(16)
         format = data[0:4].decode("ascii")
         length = int.from_bytes(data[12:14], "little")
-        print("Format: %s, Length: %i"%(format,length))
-        print("Full Header: %s"%data.hex())
+        print("Format: %s, Length: %i" % (format, length))
+        print("Full Header: %s" % data.hex())
         print("----------------------")
         i = 0
         while True:
-            i+=1
+            i += 1
             idx = in_file.read(2)
             if len(idx) == 0:                # breaks loop once no more binary data is read
                 break
             idx = int.from_bytes(idx, "little")
-            offset = struct.unpack( 'I',in_file.read(4))[0]
-            print("%i: %i"%(idx,offset))               # I also like it all in caps. 
-            if(idx > 0):
-                parts[idx]={}
+            offset = struct.unpack('I', in_file.read(4))[0]
+            if (idx > 0):
+                parts[idx] = {}
                 parts[idx]["start"] = offset
-            else:
-                print(idx)
-            if(lastIdx > 0):
+            if (lastIdx > 0):
                 parts[lastIdx]["end"] = offset
-            lastIdx=idx
-        print("i: %s len:%s"%(i,len(parts)))
+            lastIdx = idx
+        print("Parts found: i: %s len:%s" % (i, len(parts)))
         return parts
 
 
 audio_params = None
-with wave.open(folder+"SOUNDS1.WAV",'rb') as wav:
-        audio_params = wav.getparams()
+with wave.open(folder+"SOUNDS1.WAV", 'rb') as wav:
+    audio_params = wav.getparams()
 
 video_parts = parseIdx(folder+"VIDEO1.IDX")
-print(video_parts)
 sound_parts = parseIdx(folder+"SOUNDS1.IDX")
+
 
 def play_audio(idx):
     file = folder+"SOUNDS1.WAV"
@@ -54,30 +53,33 @@ def play_audio(idx):
     size = audio_params.nframes*audio_params.sampwidth*audio_params.nchannels
     bitrate = audio_params.framerate*audio_params.nchannels*audio_params.sampwidth
     # todo: this somehow isn't frame accurate (sometimes cuts off split second too early or to late)
-    start_sec=start/bitrate
-    end_sec=end/bitrate
-    duration=end_sec-start_sec
-    seek = "%.2f"%start_sec
-    duration_str="%.2f"%duration
-    run( [ 'ffplay', '-hide_banner','-loglevel','warning','-autoexit','-i',file,'-ss',seek,'-t',duration_str ])
+    start_sec = start/bitrate
+    end_sec = end/bitrate
+    duration = end_sec-start_sec
+    seek = "%.2f" % start_sec
+    duration_str = "%.2f" % duration
+    run(['ffplay', '-hide_banner', '-loglevel', 'warning',
+        '-autoexit', '-i', file, '-ss', seek, '-t', duration_str])
     return
 
 
 def play_video(idx):
     file = folder+"VIDEO1.MPG"
-    part = video_parts[idx]
-    start = part["start"]
-    end = part["end"]
-    with open(file,'rb') as fin:
-        fin.seek(start)
-        #for speedup; '-vf','setpts=0.25*PTS','-af','atempo=4',
-        run( [ 'ffplay', '-hide_banner','-loglevel','warning','-autoexit','-' ], input=fin.read(end - start))
+    if (idx in video_parts):
+        part = video_parts[idx]
+        start = part["start"]
+        end = part["end"]
+        with open(file, 'rb') as fin:
+            fin.seek(start)
+            # for speedup; '-vf','setpts=0.25*PTS','-af','atempo=4',
+            run(['ffplay', '-hide_banner', '-vf', 'scale=-1:480', '-loglevel',
+                'warning', '-autoexit', '-'], input=fin.read(end - start))
+    else:
+        print("WARNING: Video not found %i" % idx)
     return
 
 
-if(len(sys.argv) == 3 and sys.argv[1] == "v" and int(sys.argv[2])):
+if (len(sys.argv) == 3 and sys.argv[1] == "v" and int(sys.argv[2])):
     play_video(int(sys.argv[2]))
-elif(len(sys.argv) == 3 and sys.argv[1] == "a" and int(sys.argv[2])):
-    play_audio( int(sys.argv[2]))
-    
-
+elif (len(sys.argv) == 3 and sys.argv[1] == "a" and int(sys.argv[2])):
+    play_audio(int(sys.argv[2]))
